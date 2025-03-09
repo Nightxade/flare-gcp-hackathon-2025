b3075921563cd6f51d8ad7ffd256e028c81617db
Nightxade <matthewcaieatsrice@gmail.com>
2025-03-09 12:19:55 -0700

    debugging

--- src/flare_ai_rag/main.py
+++ src/flare_ai_rag/main.py
@@ -140,6 +140,7 @@ def create_app() -> FastAPI:
 
     # Load RAG data.
     df_docs = pd.read_csv(settings.data_path / "docs.csv", delimiter=",")
+    print(df_docs)
     logger.info("Loaded CSV Data.", num_rows=len(df_docs))
 
     # Set up the RAG components: 1a. Gemini Provider
